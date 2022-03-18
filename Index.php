9a6defee80ed993d6b3a0471ca17766b0edf7fb5
CassHodgkiss <93329378+CassHodgkiss@users.noreply.github.com>
2022-03-18 00:35:31 +0000

    Add files via upload

--- Index.php
+++ Index.php
@@ -1,54 +1,16 @@
-<?php 
-
-$title = "Auction Site";
-require("Header.php"); 
-
+<?php
+    $title = "Actemium";
+    include("Header.php"); 
 ?>
 
-	<div class="container">
+<div class="container">
 
-        <main role="main">
+    <main role="main">
 
-			<h1>Welcome to Auction Site</h1>
-			<h2>Login or Apply Here</h2>
+        <h1>Index</h1>
 
-			<div class="login_apply">
+    </main>
 
-				<div class="box">
+</div>
 
-					<h2>Customer</h2>
-
-					<div class="login_apply_buttons">
-
-						<a href="CustomerApply.php" class="login_apply_button"> <button class="fill_button">Apply</button> </a>
-
-						<a href="CustomerLogin.php" class="login_apply_button"> <button class="fill_button">Login</button> </a>
-
-					</div>
-						
-
-				</div>
-
-				<div class="box">
-
-					<h2>Seller</h2>
-
-					<div class="login_apply_buttons">
-
-						<a href="CustomerApply.php" class="login_apply_button"> <button class="fill_button">Apply</button> </a>
-
-						<a href="CustomerLogin.php" class="login_apply_button"> <button class="fill_button">Login</button> </a>
-						
-					</div>
-
-				</div>
-
-			</div>
-
-		</main>
-
-	</div>
-
-<?php require("Footer.php"); ?>
-
-
+<?php include("Footer.php"); ?>
